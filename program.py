483b64868448d9a01e7c07717f3186f91c56e36c
amirkle27 <amirkle@gmail.com>
2025-01-26 15:02:09 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,14 +1,17 @@
 from worker import *
 from manager import *
 
-print(Lily)
-print()
-print(Avraoom)
-print()
-print(Yossi)
-print()
-print(Yossi.CalculateSalary())
-print()
-print(Lily.get_age())
-print()
-print(Yossi.get_age())
+
+
+if __name__ == '__main__':
+    print(Lily)
+    print()
+    print(Avraoom)
+    print()
+    print(Yossi)
+    print()
+    print(Yossi.CalculateSalary())
+    print()
+    print(Lily.get_age())
+    print()
+    print(Yossi.get_age())
