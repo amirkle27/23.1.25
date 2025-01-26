8ca3cdc78f14af33955e8959f900a4a8a7e47af3
amirkle27 <amirkle@gmail.com>
2025-01-26 14:58:59 +0200

    Create program.py

---
+++ program.py
@@ -0,0 +1,14 @@
+from worker import *
+from manager import *
+
+print(Lily)
+print()
+print(Avraoom)
+print()
+print(Yossi)
+print()
+print(Yossi.CalculateSalary())
+print()
+print(Lily.get_age())
+print()
+print(Yossi.get_age())
